5d0d623887592a3a934af6de7b13c9532602b36d
ahfdkun <785440131@qq.com>
2018-04-12 23:51:22 +0800

    1、失败快速响应与重试 2、动态刷新配置

--- src/main/java/com/ahfdkun/controller/TestController.java
+++ src/main/java/com/ahfdkun/controller/TestController.java
@@ -7,6 +7,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
+// 通过http://localhost:7002/refresh接口进行动态刷新配置
+// 也可以通过github中的webhooks功能配置refresh地址动态刷新配置(需要使用ngrok工具反向代理工具实现本地web应用暴露在公网上)
 @RefreshScope
 @RestController
 public class TestController {
